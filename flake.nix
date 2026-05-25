8fe1d14150c76faadd56f90e3df4d7de45de5817
Pint Sized <fionnbennett06@gmail.com>
2026-05-25 14:36:10 +0100

    commit

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  description = "My personal Nix packages, icons, and themes";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { nixpkgs }:
+    let
+      # Define the systems you want to support
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      # A helper function to generate outputs for all systems
+      forAllSystems = nixpkgs.lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          wlogout-icons = pkgs.callPackage ./pkgs/data/wlogout-icons/icons { };
+          #sddm-theme = pkgs.callPackage ./pkgs/data/sddm-themes/homeless { };
+        }
+      );
+    };
+}
